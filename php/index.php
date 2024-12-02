9436ad32dc17bea18685cc100e4fbce6648734de
Sofia <irenesofia.lotti@studio.unibo.it>
2024-12-02 15:06:26 +0100

    added template

--- php/index.php
+++ php/index.php
@@ -1,1 +1,4 @@
-<!-- Analisi Matematica -->
+<?php 
+
+
+?>
---
+++ template/base.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Prima pagina</title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    </head>
+    <body>
+        <header>
+
+        </header>
+
+        <main>
+            <!--parte di php che cambia a
+            seconda della pagina-->
+        </main>
+
+        <footer>
+            <p>
+                LafruttadiGrimilde.com<br>
+                Contattaci<br>
+                Privacy Policy | Cookie Policy | Info Legali
+            </p>
+        </footer>
+
+    </body>
+</html>
