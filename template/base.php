e13a4ff6b0a282136b5e5d05a01786781e762622
Sofia <irenesofia.lotti@studio.unibo.it>
2025-01-21 21:06:44 +0100

    inizio css e inizio sidebar

--- template/base.php
+++ template/base.php
@@ -4,10 +4,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title><?php echo $templateParams["titolo"]; ?></title>
         <link rel="stylesheet" type="text/css" href="../css/style.css" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
         <header>
             <!--pannello per aprire canvas-->
+            <?php 
+                require("offcanvas.php");
+            ?>
+
             <h1>EMPORIO DI GRIMILDE </h1>
         </header>
 <!-- il main contiena la pagina specifica dove si trova il contenuto (tipo contenutoIndex (nota non index ma contenuto...))  -->
--- template/contenutoIndex.php
+++ template/contenutoIndex.php
@@ -1,14 +1,14 @@
 <!--immagina tutto nel main-->
 <img src="../utils/img/frutta.jpg" alt="background" />
 <form action="" method="POST">
-    <a href="acquisto.php"><input type="submit" name="acquista" value="COMPRA ORA" /></a>
+    <input type="submit" name="acquista" value="COMPRA ORA" />
 </form>
 
-<article>
+<article class="text-center">
     <h2>CHI SIAMO?</h2>
     <p>
-        L'emporio di Grimilde è un servizio di <em>spesa online</em> su cui puoi acquistare
-        prodotti sostenibili e di qualità e riceverli a casa in <em>meno di 48h.</em><br>
+        L'emporio di Grimilde è un servizio di <strong>spesa online</strong> su cui puoi acquistare
+        prodotti sostenibili e di qualità e riceverli a casa in <strong>meno di 48h.</strong><br>
         Portiamo sulla tua tavola solo prodotti autentici e genuini,
         di cui sosteniamo e valorizziamo la tracciabilità
     </p>
@@ -21,7 +21,14 @@
         <header>    <!-- questo per i contenuti delle liste da db: il foreach è per iterare la lista. templateParams contiene il risultato delle
             query. quindi per ogni recensione (che è il nome della tabella, quindi per ogni riga della tabella) prende nome, numerostelle...gitgoit -->
             <p><?php echo $recensione["Nome"]; ?> <?php echo $recensione["Cognome"]; ?></p>
-            <p><?php echo $recensione["NumeroStelle"]; ?></p>
+            <p><?php for ($i = 1; $i <= 5; $i++) {
+                if ($i <= $recensione["NumeroStelle"]) {
+                    echo '<span data-filled="true">★</span>';
+                } else {
+                    echo '<span data-filled="false">★</span>';
+                }
+            } ?>
+            </p>
         </header>
         <section>
             <p><?php echo $recensione["TestoRecensione"]; ?></p>
@@ -31,7 +38,7 @@
         </footer>
     </article>
     <?php endforeach; ?>
-    <form action="" method="POST">
+    <form action="" method="POST" class="text-end">
         <input type="submit" name="creaRecensione" value="Lascia una recensione" />
     </form>
 </article>
