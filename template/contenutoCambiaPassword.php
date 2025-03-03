bf3c69310301af2af1d70e8a7670c14f0a0e172c
Sofia <irenesofia.lotti@studio.unibo.it>
2025-03-03 15:23:08 +0100

    bootstrap

--- template/contenutoCambiaPassword.php
+++ template/contenutoCambiaPassword.php
@@ -1,22 +1,22 @@
 <div class="row justify-content-center m-0">
 <section class="temporaneo mt-5 col-10 col-md-8 col-lg-5 pb-0">
-    <h2>PROFILO</h2>
+    <h2 class="mb-3">PROFILO</h2>
     <?php if(isset($templateParams["errorelogin"])): ?>
     <p class="text-danger"><?php echo $templateParams["errorelogin"]; ?></p>
     <?php endif; ?>
     <form action="" method="POST">
-        <ul>
-            <li>
-                <label for="vecchia_password">Vecchia Password</label>
-                <input type="password" id="vecchia_password" name="vecchia_password" autocomplete="off"/>
+        <ul class="p-0 form">
+            <li class="mb-3">
+                <label for="vecchia_password" class="form-label">Vecchia Password</label>
+                <input type="password" id="vecchia_password" name="vecchia_password" autocomplete="off" class="form-control"/>
             </li>
-            <li>
-                <label for="nuova_password">Nuova Password</label>
-                <input type="password" id="nuova_password" name="nuova_password" autocomplete="off"/>
+            <li class="mb-3">
+                <label for="nuova_password" class="form-label">Nuova Password</label>
+                <input type="password" id="nuova_password" name="nuova_password" autocomplete="off" class="form-control"/>
             </li>
             <li class=" mb-3">
-                <label for="conferma_password">Conferma Password</label>
-                <input type="password" id="conferma_password" name="conferma_password" autocomplete="off"/>
+                <label for="conferma_password" class="form-label">Conferma Password</label>
+                <input type="password" id="conferma_password" name="conferma_password" autocomplete="off" class="form-control"/>
             </li>
             <li  class="text-center mb-3">
                 </label>
